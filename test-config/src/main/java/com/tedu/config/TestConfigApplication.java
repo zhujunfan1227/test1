058b2f1a4c0122cdd7d717f842f11450e4e6bf6b
朱俊范 <@DESKTOP-L4O73DU.mshome.net>
2020-02-07 11:51:42 +0800

    config

---
+++ test-config/src/main/java/com/tedu/config/TestConfigApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConfigApplication.class, args);
+	}
+
+}
