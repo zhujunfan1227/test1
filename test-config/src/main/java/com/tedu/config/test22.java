7bea0ed6cd70874d759df372261422a8c2c0bed8
zhujunfan1227 <60765799+zhujunfan1227@users.noreply.github.com>
2020-02-07 15:38:46 +0800

    Update test22.java

--- test-config/src/main/java/com/tedu/config/test22.java
+++ test-config/src/main/java/com/tedu/config/test22.java
@@ -1,1 +1,13 @@
-// 我这个方法执行么1111133333
+package com.tedu.config;
+
+
+public class test22 {
+
+	public static void main(String[] args) {
+		///6666666666777777777777
+		// 我这个方法执行么1111133333
+    //888899999999999999
+	}
+
+}
+
